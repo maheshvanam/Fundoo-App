7242d88dc6d3d06ce01e82bd3055c9e51f95ce6c
Mahesh Vanam <maheshvanam09@gmail.com>
2020-05-24 21:10:29 +0530

    [Mahesh]ADDED: added NSAttributed String logo in  login screen

--- FundooApp/ViewController.swift
+++ FundooApp/ViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let text = "Fundoo"
+        logoLabel.attributedText = self.getAttributedLogo(logoText : text)
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    func getAttributedLogo(logoText : String ) -> NSAttributedString {
+        var colorsArray : [UIColor] = [ UIColor.blue , UIColor.red , UIColor.yellow ,
+                                        UIColor.blue , UIColor.green , UIColor.red ]
+        colorsArray.append(UIColor.blue)
+        let attributedLogo = NSMutableAttributedString(string: logoText)
+        for i in 0...5 {
+            attributedLogo.addAttribute(.foregroundColor,
+                                        value: colorsArray[i], range: NSRange(location: i,length: 1))
+        }
+        return attributedLogo
     }
 }
 
