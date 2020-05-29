ae19f3c3270a101f29566bd6773dbab341743a12
Mahesh Vanam <maheshvanam09@gmail.com>
2020-05-29 21:13:32 +0530

    Added. SignUpVewPresenterDelegate

---
+++ FundooApp/Presenter/SignupViewPresenterDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SignupViewPresenterDelegate.swift
+//  FundooApp
+//
+//  Created by admin on 29/05/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+protocol SignupViewPresenterDelegate {
+    
+}
