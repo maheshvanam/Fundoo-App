7f4ca21c4e7f2dc849c90fb1dc86cd91d2aa16ab
Mahesh Vanam <maheshvanam09@gmail.com>
2020-05-29 12:41:31 +0530

    Added. testcase to test invalid password

--- FundooAppTests/FundooAppTests.swift
+++ FundooAppTests/FundooAppTests.swift
@@ -16,4 +16,11 @@ class FundooAppTests: XCTestCase {
         let result = try dataService.checkValidUserOrNot(email: "Fun", password: "Fundoo@122")
         XCTAssertEqual(Result.INVALID_EMAIL, result)
     }
+    
+    func testCoreDataService_whenGivenAInvalidPassword_ShouldReturn_InvalidPassword() throws {
+        let dataService = CoreDataServiceImpl()
+        let result = try dataService.checkValidUserOrNot(email: "fundoo@gmail.com", password: "Fundoo@122")
+        XCTAssertEqual(Result.INVALID_PASSWORD, result)
+    }
+    
 }
