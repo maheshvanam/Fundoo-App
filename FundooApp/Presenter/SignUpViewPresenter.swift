6476a3501c62868d95716fa171a52b4c30d3327d
Mahesh Vanam <maheshvanam09@gmail.com>
2020-05-29 21:11:26 +0530

    Added. SignUpVewPresenter

---
+++ FundooApp/Presenter/SignUpViewPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewPresenter.swift
+//  FundooApp
+//
+//  Created by admin on 29/05/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+class SignUpViewPresenter {
+    
+}
+
