0b8b71f202470133c99acc42ebfd6952266315e1
Mahesh Vanam <maheshvanam09@gmail.com>
2020-05-31 23:41:09 +0530

    Added. HomeControllerDelegate

---
+++ FundooApp/Home/View/HomeControllerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  HomeControllerDelegate.swift
+//  FundooApp
+//
+//  Created by admin on 31/05/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeControllerDelegate {
+    func handleMenuToggle()
+}
