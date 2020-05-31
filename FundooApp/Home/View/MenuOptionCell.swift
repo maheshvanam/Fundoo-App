c9d8dc2fe04ea549774815726431f9905fffca6e
Mahesh Vanam <maheshvanam09@gmail.com>
2020-06-01 01:50:15 +0530

    Added. labels for menu options

--- FundooApp/Home/View/MenuOptionCell.swift
+++ FundooApp/Home/View/MenuOptionCell.swift
@@ -18,6 +18,14 @@ class MenuOptionCell: UITableViewCell {
         return iv
     }()
     
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Simple Text"
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .darkGray
@@ -27,6 +35,12 @@ class MenuOptionCell: UITableViewCell {
         iconImageView.leftAnchor.constraint(equalTo: leftAnchor,constant: 12).isActive = true
         iconImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
         iconImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor,constant: 12).isActive = true
+        
     }
     
     required init?(coder: NSCoder) {
