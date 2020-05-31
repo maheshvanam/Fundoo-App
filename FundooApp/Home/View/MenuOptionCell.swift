4c34b335b51dfbb566d098f9803735024fe93c30
Mahesh Vanam <maheshvanam09@gmail.com>
2020-06-01 01:46:15 +0530

    Added. imageview for menu options

--- FundooApp/Home/View/MenuOptionCell.swift
+++ FundooApp/Home/View/MenuOptionCell.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class MenuOptionCell: UITableViewCell {
-
+    
+    let iconImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = .blue
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .darkGray
+        addSubview(iconImageView)
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        iconImageView.leftAnchor.constraint(equalTo: leftAnchor,constant: 12).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
     }
     
     required init?(coder: NSCoder) {
