505c45853a0ecba4c4e5ea732103a56c5758d16c
Mahesh Vanam <maheshvanam09@gmail.com>
2020-06-21 18:50:59 +0530

    Added. presenter for the Add Label ViewController

---
+++ FundooApp/Label/AddLabelPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  AddLabelPresenter.swift
+//  FundooApp
+//
+//  Created by admin on 21/06/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
---
+++ FundooApp/Label/AddLabelViewDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AddLabelViewDelegate.swift
+//  FundooApp
+//
+//  Created by admin on 21/06/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
