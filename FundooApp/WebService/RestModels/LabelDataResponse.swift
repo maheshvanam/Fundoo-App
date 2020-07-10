c2c97c90c2a770f67b8455731c34776412b40ab4
Mahesh Vanam <maheshvanam09@gmail.com>
2020-07-10 12:37:07 +0530

    Added. LabelDataResponse is created to get the data response as object

---
+++ FundooApp/WebService/RestModels/LabelDataResponse.swift
@@ -0,0 +1,15 @@
+//
+//  LabelDataResponse.swift
+//  FundooApp
+//
+//  Created by admin on 10/07/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+struct LabelDataResponse {
+    var data = ""
+    var message = ""
+    var details:[LabelResponse] = []
+}
