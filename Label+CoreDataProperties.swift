fd168ceec2565bae088ae608b3d92e480427c1cd
Mahesh Vanam <maheshvanam09@gmail.com>
2020-07-17 21:56:40 +0530

    Refactor. recovered necessary files

---
+++ Label+CoreDataProperties.swift
@@ -0,0 +1,41 @@
+//
+//  Label+CoreDataProperties.swift
+//  FundooApp
+//
+//  Created by admin on 29/06/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Label {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Label> {
+        return NSFetchRequest<Label>(entityName: "Label")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var notes: NSSet?
+    @NSManaged public var user: User?
+
+}
+
+// MARK: Generated accessors for notes
+extension Label {
+
+    @objc(addNotesObject:)
+    @NSManaged public func addToNotes(_ value: Note)
+
+    @objc(removeNotesObject:)
+    @NSManaged public func removeFromNotes(_ value: Note)
+
+    @objc(addNotes:)
+    @NSManaged public func addToNotes(_ values: NSSet)
+
+    @objc(removeNotes:)
+    @NSManaged public func removeFromNotes(_ values: NSSet)
+
+}
---
+++ User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  FundooApp
+//
+//  Created by admin on 29/06/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class User: NSManagedObject {
+
+}
